eb7c76f4242b833e1cdbbc427fadd24ec41596f5
Tim Biles <timbilestimbiles@gmail.com>
2018-10-08 21:39:37 -0500

    more characters added

--- controllers/characterCtrl.js
+++ controllers/characterCtrl.js
@@ -1,8 +1,12 @@
 const characters = require('../data/characters.json');
 
 const getCharacters = (req, res) => {
-
-  res.status(200).send(characters);
+  let og = [];
+  characters.map(e => {
+    e.games.map(el => el.includes('N64') && og.push(e));
+  });
+  console.log(og);
+  res.status(200).send(og);
 };
 
 module.exports = {
